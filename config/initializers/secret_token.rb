715b6245da7c59a8c193c5ff934cf6f02655798b
Ivan Storck <ivanoats@gmail.com>
2013-06-05 11:14:00 -0700

    initial rails app with user scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmailExercise::Application.config.secret_token = '390b9d5f55379138e3b8bde1746296b686c797e1823f658af9c1776b1d35c0b6c3025264c00bcf31f852e05bd3307fcecb620a4acf43921e8deb44413cb1161f'
